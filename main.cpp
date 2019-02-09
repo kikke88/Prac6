9bab43ff840f3d06ac4dce489f97fed7c1ff3f62
Mokrov Kirill <kikke88@yandex.ru>
2019-02-09 14:14:44 +0300

    fix1

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ int main(int argc, char* argv[])//n k
 {
 	int n = stoi(argv[1]),
 		k = stoi(argv[2]),
-		num_of_elem = pow(2, n),
-		sum = 0;
+		num_of_elem = pow(2, n);
+	double sum = 0;
 	complexd* qbit_vec = new complexd[num_of_elem];
 	unsigned cur_time = time(0);
 #pragma omp parallel for shared(qbit_vec, cur_time) reduction(+:sum) schedule(dynamic)	
@@ -23,13 +23,14 @@ int main(int argc, char* argv[])//n k
 		qbit_vec[i].real(rand() / RAND_MAX_D);
 		qbit_vec[i].imag(rand() / RAND_MAX_D);
 		sum += abs(qbit_vec[i] * qbit_vec[i]);
+//		cout << qbit_vec[i] << endl;
 	}
 	sum = pow(sum, 0.5);
-
+//	cout << "_____" << sum << "_____" << endl;
 #pragma omp parallel for shared(sum, qbit_vec) schedule(dynamic)	
 	for (int i = 0; i < num_of_elem; i += 1) {
 		qbit_vec[i] /= sum;	
-	std::cout << qbit_vec[i] << std::endl;
+//		std::cout << qbit_vec[i] << std::endl;
 	}
 	delete[] qbit_vec;
 	return 0;
