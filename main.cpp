14bad9ba922241ea257df327825b50adaf871e99
Mokrov Kirill <kikke88@yandex.ru>
2019-02-09 19:54:03 +0300

    + quantum transformation

--- main.cpp
+++ main.cpp
@@ -9,6 +9,28 @@ using namespace std;
 typedef complex<double> complexd;
 const double RAND_MAX_D = RAND_MAX;
 
+
+void quantum_transformation(const complexd* a, const int n, const int num_of_elem, const double u[][2], const int k)
+{
+	for (int i = 0; i < num_of_elem; ++i) {
+		int tmp1 = i & ~(1 << (n - k)),
+			tmp2 = i | 1 << (n - k);
+		cout << i << "     " << tmp1 << "   " << tmp2 << endl;
+	}
+	complexd* result = new complexd[num_of_elem];
+#pragma omp parallel for shared(a, u, result)// schedule(dynamic)
+	for (int i = 0; i < num_of_elem; i += 1) {
+		int i_k = i >> (n - k) && 1;
+		result[i] = u[i_k][0] * a[i & ~(1 << (n - k))] + u[i_k][1] * a[i | 1 << (n - k)];
+	}
+	for (int i = 0; i < num_of_elem; ++i) {
+		cout << result[i] << endl;
+	}
+	delete[] result;
+}
+
+
+
 int main(int argc, char* argv[])//n k
 {
 	int n = stoi(argv[1]),
@@ -17,21 +39,27 @@ int main(int argc, char* argv[])//n k
 	double sum = 0;
 	complexd* qbit_vec = new complexd[num_of_elem];
 	unsigned cur_time = time(0);
-#pragma omp parallel for shared(qbit_vec, cur_time) reduction(+:sum) schedule(dynamic)	
+#pragma omp parallel for default(none) shared(qbit_vec, cur_time, num_of_elem) reduction(+:sum)// schedule(dynamic)	
 	for (int i = 0; i < num_of_elem; i += 1) {
 		srand(cur_time + i);
-		qbit_vec[i].real(rand() / RAND_MAX_D);
-		qbit_vec[i].imag(rand() / RAND_MAX_D);
+		qbit_vec[i].real(i);
+		qbit_vec[i].imag(0);
+		//qbit_vec[i].real(rand() / RAND_MAX_D);
+		//qbit_vec[i].imag(rand() / RAND_MAX_D);
 		sum += abs(qbit_vec[i] * qbit_vec[i]);
-//		cout << qbit_vec[i] << endl;
 	}
 	sum = pow(sum, 0.5);
-//	cout << "_____" << sum << "_____" << endl;
-#pragma omp parallel for shared(sum, qbit_vec) schedule(dynamic)	
+#pragma omp parallel for default(none) shared(sum, qbit_vec, num_of_elem)// schedule(dynamic)	
 	for (int i = 0; i < num_of_elem; i += 1) {
 		qbit_vec[i] /= sum;	
-//		std::cout << qbit_vec[i] << std::endl;
 	}
+	for (int i = 0; i < num_of_elem; ++i) {
+		cout << qbit_vec[i] << endl;
+	}
+	double tmp = 1 / sqrt(2);
+	double matrix[2][2] = {{tmp, tmp},
+							{tmp, - tmp}};
+	quantum_transformation(qbit_vec, n, num_of_elem, matrix, k);
 	delete[] qbit_vec;
 	return 0;
 }
