39d9c184055635ef90edaceca1772728c98612c9
Mokrov Kirill <kikke88@yandex.ru>
2019-02-13 16:33:09 +0300

    nice

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <complex>
 #include <cmath>
 #include <cstdlib>
+#include <fstream>
 
 #include <omp.h>
 
@@ -10,56 +11,59 @@ typedef complex<double> complexd;
 const double RAND_MAX_D = RAND_MAX;
 
 
-void quantum_transformation(const complexd* a, const int n, const int num_of_elem, const double u[][2], const int k)
+void quantum_transformation(const complexd* a, complexd* result, const int n, const int num_of_elem, const double u[][2], const int k, const double sum)
 {
-	for (int i = 0; i < num_of_elem; ++i) {
-		int tmp1 = i & ~(1 << (n - k)),
-			tmp2 = i | 1 << (n - k);
-		cout << i << "     " << tmp1 << "   " << tmp2 << endl;
-	}
-	complexd* result = new complexd[num_of_elem];
-#pragma omp parallel for shared(a, u, result)// schedule(dynamic)
+	int test = 1 << (n - k);
+#pragma omp parallel for default(none) shared(a, u, result, test)
 	for (int i = 0; i < num_of_elem; i += 1) {
-		int i_k = i >> (n - k) && 1;
-		result[i] = u[i_k][0] * a[i & ~(1 << (n - k))] + u[i_k][1] * a[i | 1 << (n - k)];
+		result[i] = (u[i & test && 1][0] * a[i & ~(test)] + u[i & test && 1][1] * a[i | test]) / sum;
 	}
+/*
 	for (int i = 0; i < num_of_elem; ++i) {
 		cout << result[i] << endl;
 	}
-	delete[] result;
+*/
 }
 
-
-
-int main(int argc, char* argv[])//n k
+int main(int argc, char* argv[])//n, k, t
 {
-	int n = stoi(argv[1]),
-		k = stoi(argv[2]),
-		num_of_elem = pow(2, n);
-	double sum = 0;
+	bool test = false;
+	double prog_time1 = 0, prog_time2 = 0;
+	int num_of_start = 1;
+	int n = strtol(argv[1], NULL, 10),
+		k = strtol(argv[2], NULL, 10),
+		num_of_elem = 1 << n;
+	int thrds = strtol(argv[3], NULL, 10);
+	omp_set_num_threads(thrds);
 	complexd* qbit_vec = new complexd[num_of_elem];
-	unsigned cur_time = time(0);
-#pragma omp parallel for default(none) shared(qbit_vec, cur_time, num_of_elem) reduction(+:sum)// schedule(dynamic)	
-	for (int i = 0; i < num_of_elem; i += 1) {
-		srand(cur_time + i);
-		qbit_vec[i].real(i);
-		qbit_vec[i].imag(0);
-		//qbit_vec[i].real(rand() / RAND_MAX_D);
-		//qbit_vec[i].imag(rand() / RAND_MAX_D);
-		sum += abs(qbit_vec[i] * qbit_vec[i]);
-	}
-	sum = pow(sum, 0.5);
-#pragma omp parallel for default(none) shared(sum, qbit_vec, num_of_elem)// schedule(dynamic)	
-	for (int i = 0; i < num_of_elem; i += 1) {
-		qbit_vec[i] /= sum;	
-	}
-	for (int i = 0; i < num_of_elem; ++i) {
-		cout << qbit_vec[i] << endl;
-	}
+	complexd* result = new complexd[num_of_elem];
 	double tmp = 1 / sqrt(2);
-	double matrix[2][2] = {{tmp, tmp},
-							{tmp, - tmp}};
-	quantum_transformation(qbit_vec, n, num_of_elem, matrix, k);
+	double matrix[2][2] = {{tmp, tmp}, {tmp, - tmp}};
+	for (int counter = 0; counter < num_of_start; ++counter) {
+		double sum = 0;
+		unsigned int thrd_seed;
+		prog_time1 -= omp_get_wtime();
+#pragma omp parallel  default(none) shared(qbit_vec, num_of_elem, sum) private(thrd_seed)
+		{
+			thrd_seed = omp_get_thread_num();
+#pragma omp for reduction(+:sum)
+			for (int i = 0; i < num_of_elem; i += 1) {
+				qbit_vec[i] = complexd(rand_r(&thrd_seed), rand_r(&thrd_seed));
+				//qbit_vec[i].real(rand_r(&thrd_seed));
+				//qbit_vec[i].imag(rand_r(&thrd_seed));
+				sum += norm(qbit_vec[i]);		
+			}	
+		}	
+		sum = pow(sum, 0.5);
+		prog_time1 += omp_get_wtime();
+		prog_time2 -= omp_get_wtime();
+		quantum_transformation(qbit_vec, result, n, num_of_elem, matrix, k, sum);
+		prog_time2 += omp_get_wtime();
+	}
 	delete[] qbit_vec;
+	delete[] result;
+
+	ofstream ofile("out", ios::app);
+	ofile << n << "\t" << k << "\t"  << thrds << "\t" <<  prog_time1 / num_of_start << "@@@ " << prog_time2 / num_of_start << "  SUM " << (prog_time1 + prog_time2) / num_of_start << endl;	
 	return 0;
 }
