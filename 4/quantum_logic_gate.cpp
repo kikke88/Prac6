89475b964815adfdcfbeaefa70d98f13da5e17e0
Мокров Кирилл <s02160123@dazhbog.cmc.msu.ru>
2019-03-29 11:56:45 +0300

    ..

--- 4/quantum_logic_gate.cpp
+++ 4/quantum_logic_gate.cpp
@@ -8,20 +8,21 @@ class Quantum_vector {
 private:
     const int num_of_qubits;
     const int num_of_elem;
-    const int size;//
+    const int size;
     const int rank;
     const int elem_in_one_proc;
     MPI_Datatype double_double;//*
     complexd* vector_1;//*
     complexd* vector_2;//*
-    void file_manipulation(const string, int);
+    void file_manipulation(const char*, int);//
 public:
     Quantum_vector(int, int, int);
     ~Quantum_vector();
     void vector_filling();
-    void vector_filling(string);
+    void vector_filling(const char*);
 
     void Hadamard_gate(int);
+    void n_Hadamard_gate();
 
 
 };
@@ -76,8 +77,26 @@ void Quantum_vector::vector_filling()
     }
 }
 
-void Quantum_vector::file_manipulation(const string )
-void Quantum_vector::vector_filling(const string input_file_name)
+void Quantum_vector::file_manipulation(const char* filename, int flag)
+{
+	MPI_Datatype subarr_type;
+	MPI_File file;
+	int start = elem_in_one_proc * rank;
+	MPI_Type_create_subarray(1, &num_of_elem, &elem_in_one_proc, &start, MPI_ORDER_C, double_double, &subarr_type);
+	MPI_Type_commit(&subarr_type);
+	if (flag) {
+		MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &file);
+		MPI_File_set_view(file, 0, double_double, subarr_type, "native", MPI_INFO_NULL);
+		MPI_File_read_all(file, vector_1, elem_in_one_proc, double_double, MPI_STATUS_IGNORE);
+	} else {
+		MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &file);
+		MPI_File_set_view(file, 0, double_double, subarr_type, "native", MPI_INFO_NULL);
+		MPI_File_write_all(file, vector_1, elem_in_one_proc, double_double, MPI_STATUS_IGNORE);
+	}
+	MPI_Type_free(&subarr_type);
+}
+
+void Quantum_vector::vector_filling(const char* input_file_name)
 {
     int input_flag = 1;
     file_manipulation(input_file_name, input_flag);
@@ -85,5 +104,43 @@ void Quantum_vector::vector_filling(const string input_file_name)
 
 void Quantum_vector::Hadamard_gate(int k)
 {
+	double tmp = 1 / sqrt(2);
+	double H[4] = {tmp, tmp, tmp, -tmp};
+	int test = 1 << (num_of_qubits - k);
+	int log_k = log2(size);
+	if (k > log_k) {
+#pragma omp parallel for
+		for (int i = 0; i < elem_in_one_proc; ++i) {
+			vector_2[i] = H[(i & test && 1) * 2] * vector_1[i & ~(test)]
+						+ H[(i & test && 1) * 2 + 1] * vector_1[i | test];
+		}
+	} else {
+		complexd* recv_vec = new complexd[elem_in_one_proc];
+		int dest_rank = rank ^ 1 << (log_k - k);
+		int swap_bit = rank & 1 << (log_k - k) && 1;
+		if (swap_bit == 1) {
+			MPI_Send(vector_1, elem_in_one_proc, double_double, dest_rank, 0 , MPI_COMM_WORLD);
+			MPI_Recv(recv_vec, elem_in_one_proc, double_double, dest_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			swap(vector_1, recv_vec);
+		} else {
+			MPI_Recv(recv_vec, elem_in_one_proc, double_double, dest_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Send(vector_1, elem_in_one_proc, double_double, dest_rank, 0 , MPI_COMM_WORLD);
+		}
+#pragma omp parallel for
+		for (int i = 0; i < elem_in_one_proc; ++i) {
+			vector_2[i] = H[swap_bit * 2] * vector_1[i] + H[swap_bit * 2 + 1] * recv_vec[i];
+		}
+		if (swap_bit == 1) {
+			recv_vec = vector_1;
+		}
+		delete[] recv_vec;
+	}
+	return;	
+}
 
+void Quantum_vector::n_Hadamard_gate()
+{
+	
+	
+	
 }
